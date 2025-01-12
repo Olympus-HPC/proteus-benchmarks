5c823a3ad95492f8fa5a2f835cb9c6a5deebaa51
Giorgis Georgakoudis <georgakoudis1@llnl.gov>
2025-01-12 01:46:18 -0800

    Quiet wget

--- setup/install-miniconda3.sh
+++ setup/install-miniconda3.sh
@@ -11,7 +11,7 @@ function setup() {
   export CONDA_ENVS_DIR=${MINICONDA_DIR}/envs
   export CONDA_PKGS_DIR=${MINICONDA_DIR}/pkgs
 
-  wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$(uname -m).sh -O ./${MINICONDA_DIR}/miniconda.sh
+  wget -q https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-$(uname -m).sh -O ./${MINICONDA_DIR}/miniconda.sh
   bash ./${MINICONDA_DIR}/miniconda.sh -b -u -p ./${MINICONDA_DIR}
   rm ./${MINICONDA_DIR}/miniconda.sh
   source ./${MINICONDA_DIR}/bin/activate
